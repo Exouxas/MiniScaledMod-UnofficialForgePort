a9aa12021f3d6ba0c15fd4e13ea50a28480823af
Exouxas <exouxas@gmail.com>
2026-02-28 00:00:13 +0100

    Refactor tellClientToInvoke method to accept variable arguments for improved flexibility

--- src/ipstubs/java/qouteall/q_misc_util/api/McRemoteProcedureCall.java
+++ src/ipstubs/java/qouteall/q_misc_util/api/McRemoteProcedureCall.java
@@ -1,12 +1,11 @@
 package qouteall.q_misc_util.api;
 
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 
 public class McRemoteProcedureCall {
     public static void tellServerToInvoke(String callableName, Object... args) {
     }
 
-    public static void tellClientToInvoke(ServerPlayer player, String callableName, CompoundTag tag) {
+    public static void tellClientToInvoke(ServerPlayer player, String callableName, Object... args) {
     }
 }
